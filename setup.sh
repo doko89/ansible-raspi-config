9f7baca16d07a21d180b6a2578e0242c134f07e2
doko <sundoko@gmail.com>
2019-07-11 17:08:17 +0700

    loop menu

--- setup.sh
+++ setup.sh
@@ -26,6 +26,31 @@ echo "3. isntall mikbotam"
 echo "4. install mikhmon"
 echo "5. install dns server (raspberry pi ubuntu arm only)"
 read -p "pilih : " pilih
+func
+}
+
+func (){
+if [ "$pilih" -eq 1 ];then
+        ansible-playbook -i source/inventory source/websrv.yml --extra-vars="interpreter_python= auto_silent"
+	menu
+elif [ "$pilih" -eq 2 ];then
+        ansible-playbook -i source/inventory source/mysql.yml --extra-vars="interpreter_python= auto_silent"
+	menu
+elif [ "$pilih" -eq 3 ];then
+        read -p "Please Insert domain : (ex: mikbotam.domain.com) " domain
+        ansible-playbook -i source/inventory source/mikbotam.yml --extra-vars="interpreter_python= auto_silent, domain=$domain"
+	menu
+elif [ "$pilih" -eq 4 ];then
+        read -p "Please Insert domain : (ex: mikhmon.domain.com) " domain
+        ansible-playbook -i source/inventory source/mikhmon.yml --extra-vars="interpreter_python= auto_silent, domain=$domain"
+	menu
+elif [ "$pilih" -eq 5 ];then
+        ansible-playbook -i source/inventory source/dns.yml --extra-vars="interpreter_python= auto_silent"
+	menu
+else
+        echo "pilihan yang anda masukkan salah"
+        exit 0;
+fi
 }
 
 echo "starting..."
@@ -35,22 +60,4 @@ dep
 echo "starting..."
 clear >$(tty)
 menu
-
-if [ "$pilih" -eq 1 ];then
-	ansible-playbook -i source/inventory source/websrv.yml --extra-vars="interpreter_python= auto_silent"
-elif [ "$pilih" -eq 2 ];then
-	        ansible-playbook -i source/inventory source/mysql.yml --extra-vars="interpreter_python= auto_silent"
-elif [ "$pilih" -eq 3 ];then
-	read -p "Please Insert domain : (ex: mikbotam.domain.com) " domain
-	ansible-playbook -i source/inventory source/mikbotam.yml --extra-vars="interpreter_python= auto_silent, domain=$domain"
-elif [ "$pilih" -eq 4 ];then
-	read -p "Please Insert domain : (ex: mikhmon.domain.com) " domain
-        ansible-playbook -i source/inventory source/mikhmon.yml --extra-vars="interpreter_python= auto_silent, domain=$domain"
-elif [ "$pilih" -eq 5 ];then
-        ansible-playbook -i source/inventory source/dns.yml --extra-vars="interpreter_python= auto_silent"
-else
-	echo "pilihan yang anda masukkan salah"
-	exit 0;
-fi
 	
-menu
